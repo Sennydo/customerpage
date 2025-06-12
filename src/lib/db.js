ed7cf1c1180a09b995ced76686694a1ec2087232
Sennydo <sadhvikchelmas1312@gmail.com>
2025-06-12 14:31:07 +0530

    Setting up connection for SQL

---
+++ src/lib/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+try {
+
+} catch (error) {
+    throw Error(error.message);
+    return {error};
+}
